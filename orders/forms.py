603f47fa05f79c75146d53d6e3be079cda63c5ef
konevre <konevre@mail.ru>
2022-08-21 14:35:49 +0700

    initial commit

---
+++ orders/forms.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from django import forms
+from .models import Order
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'postal_code', 'city']
